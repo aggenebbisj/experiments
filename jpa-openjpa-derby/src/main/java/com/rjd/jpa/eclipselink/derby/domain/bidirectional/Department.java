77320febf2043e1ba2cb404a8fe3b201b6b7cd2d
Remko de Jong <aggenebbisj@gmail.com>
2014-09-11 23:59:59 +0200

    added jpa project with openjpa

---
+++ jpa-openjpa-derby/src/main/java/com/rjd/jpa/eclipselink/derby/domain/bidirectional/Department.java
@@ -0,0 +1,63 @@
+package com.rjd.jpa.eclipselink.derby.domain.bidirectional;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Department implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    //    @JoinColumn(name = "DEPARTMENT_ID")
+    private List<Employee> employees = new ArrayList<Employee>();
+
+    public Department() {
+        super();
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee) {
+//        employee.setDepartment(this);
+        this.employees.add(employee);
+    }
+}
---
+++ jpa-openjpa-derby/src/main/java/com/rjd/jpa/eclipselink/derby/domain/unidirectional/Person.java
@@ -0,0 +1,63 @@
+package com.rjd.jpa.eclipselink.derby.domain.unidirectional;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Person implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "PERSON_ID", referencedColumnName = "ID", nullable = false) // nullable is false needed
+                                                                                   // to prevent foreign key 
+                                                                                   // not null violation
+    private List<Car> cars = new ArrayList<Car>();
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void addCar(Car car) {        
+        this.cars.add(car);
+    }
+}
