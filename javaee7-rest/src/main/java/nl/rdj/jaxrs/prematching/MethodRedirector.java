6d439e530311ddf53303ac8d59967a8662e94dc6
Remko de Jong <aggenebbisj@gmail.com>
2014-09-14 22:36:33 +0200

    turned off method rewriting

--- javaee7-rest/src/main/java/nl/rdj/jaxrs/prematching/MethodRedirector.java
+++ javaee7-rest/src/main/java/nl/rdj/jaxrs/prematching/MethodRedirector.java
@@ -13,8 +13,8 @@ public class MethodRedirector implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        System.out.println("-->" + requestContext.getMethod());
-        requestContext.setMethod(HttpMethod.POST); 
+//        System.out.println("-->" + requestContext.getMethod());
+//        requestContext.setMethod(HttpMethod.POST); 
     }
 
 }
